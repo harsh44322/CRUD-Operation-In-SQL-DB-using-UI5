c1135a3520a27e0b83d54a72b1d1d9cf4d33c370
harsh44322 <harsh88588@gmail.com>
2024-07-18 16:09:35 +0530

    Added AJAX call to get data from DB

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("postgresql.controller.Main", {
             onInit: function () {
+                var oModel = new JSONModel();
 
-            }
+                this.getView().setModel(oModel);
+
+                this.loadData();
+            },
+            loadData: function() {
+                var that = this;
+                
+                $.ajax({
+                  url: "http://localhost:3000/data",
+                  success: function(data) {
+                    that.getView().getModel().setData(data);
+                    console.log(data);
+                  },
+                  error: function() {
+                    alert("Failed to fetch data from server.");
+                  }
+                });
+              }
         });
     });
