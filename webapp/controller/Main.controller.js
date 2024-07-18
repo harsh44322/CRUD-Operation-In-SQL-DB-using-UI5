8dc61dd3d09c34d907ea721483f0d73da9d1b523
harsh44322 <harsh88588@gmail.com>
2024-07-18 23:20:20 +0530

    made changes in ajax call

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,29 +10,39 @@ sap.ui.define([
 
         return Controller.extend("postgresql.controller.Main", {
             onInit: function () {
-                var oModel = new JSONModel();
+                // var oModel = new JSONModel();
 
-                this.getView().setModel(oModel);
+                // this.getView().setModel(oModel);
 
                 this.loadData();
             },
             loadData: function() {
-                var that = this;
-                
-                var aData = $.ajax({
-                    url: "http://localhost:3000/data",
-                    type: "GET",
-                    success: function(data) {
-                      console.log("Data received:", data);
-                      var oModel = new sap.ui.model.json.JSONModel();
-                      oModel.setData(data);
-                      this.getView().setModel(oModel);
-                    }.bind(this),
-                    error: function(xhr, status, error) {
-                      console.error("Failed to fetch data:", error);
-                      alert("Failed to fetch data from server.");
-                    }
-                  });
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel);
+
+                // Fetch data from the backend API
+                fetch('http://localhost:3000/data')
+                    .then(response => response.json())
+                    .then(data => {
+                    oModel.setData(data);
+                    })
+                    .catch(err => {
+                    console.error('Error fetching data:', err);
+                    });
+                // var aData = $.ajax({
+                //     url: "http://localhost:3000/data",
+                //     type: "GET",
+                //     success: function(result) {
+                //       console.log("Data received:", result);
+                //       var oModel = new sap.ui.model.json.JSONModel();
+                //       oModel.setData(result);
+                //       this.getView().setModel(oModel);
+                //     }.bind(this),
+                //     error: function(xhr, status, error) {
+                //       console.error("Failed to fetch data:", error);
+                //       alert("Failed to fetch data from server.");
+                //     }
+                //   });
               }
         });
     });
