7b2f82708b0f7574e9e90697c101a89888281639
harsh44322 <harsh88588@gmail.com>
2024-07-18 23:06:12 +0530

    made changes in ajax call

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -24,16 +24,12 @@ sap.ui.define([
                     method: "GET",
                     success: function(data) {
                       console.log("Data received:", data);
-                      // Handle data in your SAPUI5 application
-                      // Assuming you have a JSONModel initialized in your controller
                       var oModel = new sap.ui.model.json.JSONModel();
                       oModel.setData(data);
-                      // Set the model to your view or control
                       this.getView().setModel(oModel);
                     }.bind(this),
                     error: function(xhr, status, error) {
                       console.error("Failed to fetch data:", error);
-                      // Handle error in your SAPUI5 application
                       alert("Failed to fetch data from server.");
                     }
                   });
