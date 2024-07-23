cb0f047ff7d55cc822a635b3a2f6ae98fe74da91
harsh44322 <harsh88588@gmail.com>
2024-07-23 22:26:43 +0530

    added logic to save data to backend

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -12,24 +12,75 @@ sap.ui.define([
             onInit: function () {
 
                 this.loadData();
+                var oModel = new JSONModel({
+                    rows: []
+                });
+                this.getView().setModel(oModel);
 
             },
             loadData: function() {
                 var oModel = new JSONModel();
                 this.getView().setModel(oModel);
-      
+                // oModel.setBusy(true);
                 var aData = $.ajax({
                     url: "http://localhost:3000/data",
                     method: "GET",
                     success: function(result) {
                       oModel.setData({data: result});
                       this.getView().setModel(oModel);
+                    //   oModel.setBusy(false);
                     }.bind(this),
                     error: function(xhr, status, error) {
                       console.error("Failed to fetch data:", error);
                       alert("Failed to fetch data from server.");
                     }
-                  });
-              }
+                });
+            },
+            onCreate: function () {
+                // Get the model
+                var oModel = this.getView().getModel();
+                var aData = oModel.getProperty("/data");
+    
+                // Add a new row
+                aData.unshift({
+                    id: "",
+                    first_name: "",
+                    last_name: "",
+                    email: "",
+                    hire_date: "",
+                    salary: ""
+                });
+    
+                // Update the model
+                oModel.setProperty("/rows", aData);
+            },
+            onInputChange: function (oEvent) {
+                var oModel = this.getView().getModel();
+                var oContext = oEvent.getSource().getBindingContext();
+                var sPath = oContext.getPath();
+                var oEditedRow = oModel.getProperty(sPath);
+                oModel.setProperty("/editedRow", oEditedRow);
+            },
+            onSave: function(){
+                var oModel = this.getView().getModel();
+                var oEditedRow = oModel.getProperty("/editedRow");
+                if (!oEditedRow) {
+                    MessageToast.show("No edited row to save");
+                    return;
+                }
+    
+                $.ajax({
+                    url: "http://localhost:3000/data",
+                    method: "POST",
+                    contentType: "application/json",
+                    data: JSON.stringify(oEditedRow),
+                    success: function (response) {
+                        MessageToast.show("Row saved successfully");
+                    },
+                    error: function (error) {
+                        MessageToast.show("Error saving row");
+                    }
+                });
+            }
         });
     });
