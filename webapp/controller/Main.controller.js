651ee7f9a5e608ae847b4da3b5185ef51e1c8206
harsh44322 <harsh88588@gmail.com>
2024-07-24 22:28:19 +0530

    added logic to get last id from db

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -22,7 +22,6 @@ sap.ui.define([
             loadData: function() {
                 var oModel = new JSONModel();
                 this.getView().setModel(oModel);
-                // oModel.setBusy(true);
                 var aData = $.ajax({
                     url: "http://localhost:3000/data",
                     method: "GET",
